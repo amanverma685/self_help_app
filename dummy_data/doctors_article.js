bcdeefdd0774c03db71161fe2f96cedd7284b762
amanverma685 <av36232@gmail.com>
2023-03-24 12:07:16 +0530

    webview implemented

--- dummy_data/doctors_article.js
+++ dummy_data/doctors_article.js
@@ -38,7 +38,7 @@ export default [
         "title":"Bhagat Singh",
         "artist": 'deadmau5',
         "author":"XYZ",
-        "reading_text":"",
+        // "reading_text":"",
         "imageURL":"https://www.shutterstock.com/image-illustration/sleep-music-youtube-thumbnail-illustration-260nw-2175138261.jpg",
         "url":"https://en.wikipedia.org/wiki/Bhagat_Singh"
     },{
@@ -49,9 +49,9 @@ export default [
         "title":"ABC",
         "artist": 'deadmau5',
         "author":"XYZ",
-        "reading_text":"AKJDSG asjd sakdj asjkd haskd askdj ashkd askd hasdkjj adasdaskjd askfjsah asfhdasj dhasd haskjdas hdja hdasjdj hasjdha dadhiu ioad a",
+        // "reading_text":"AKJDSG asjd sakdj asjkd haskd askdj ashkd askd hasdkjj adasdaskjd askfjsah asfhdasj dhasd haskjdas hdja hdasjdj hasjdha dadhiu ioad a",
         "duration": 402,
         "imageURL":"https://www.shutterstock.com/image-illustration/sleep-music-youtube-thumbnail-illustration-260nw-2175138261.jpg",
-        "url":"https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/8e9f544c-5da9-4c27-a25d-f13fafcebf61/audio/128/default.mp3"
+        "url":"https://practice.geeksforgeeks.org/problems/5c2734730cb1e98e3877a0b91f7d680d0efc8acf/1"
     },
 ]
--- screens/VideoPlayerScreen.js
+++ screens/VideoPlayerScreen.js
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
-const VideoPlayerScreen = () => {
+export default function VideoPlayerScreen () {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
-    <View>
-      <Text>VideoPlayerScreen</Text>
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
-  )
+  );
 }
 
-export default VideoPlayerScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
